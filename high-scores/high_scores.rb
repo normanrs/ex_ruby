85dcc4dace2035ffc3c3f11660303076ca3d3ee7
normanrs <normanrs@live.com>
2019-03-18 12:31:32 -0600

    Adds high score exercise

---
+++ high-scores/high_scores.rb
@@ -0,0 +1,21 @@
+class HighScores 
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest 
+    @scores.last
+  end
+
+  def personal_best 
+    @scores.max
+  end
+
+  def personal_top_three 
+    sorted = @scores.sort
+    sorted.last(3).reverse
+  end
+
+end
