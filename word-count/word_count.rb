8c18414f6cb62d580a91b48db9f5e7389966ad53
normanrs <normanrs@live.com>
2019-03-18 21:04:10 -0600

    Adds word count exercise

---
+++ word-count/word_count.rb
@@ -0,0 +1,24 @@
+class Phrase 
+  attr_reader :word_count
+
+  def initialize(string_in)
+    @words = string_in.downcase.gsub(/\n/, '').split(/,\s|,|\s/)
+    @words_hash = Hash.new(0)
+  end
+
+  def word_count 
+    @words.each do |word| 
+      clean = wash(word)
+      @words_hash[clean] += 1 if clean.length > 0
+    end
+    @words_hash
+  end
+
+  def wash(word_in) 
+    better = word_in.gsub(/[^a-z0-9']/, '')
+    better[0] = '' if better[0] == "'"
+    better[-1] = '' if better[-1] == "'"
+    better
+  end
+
+end
