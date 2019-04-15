f2a63fc7972ba61cdbd866a0f6c311689b5de4d0
normanrs <normanrs@live.com>
2019-04-15 14:38:29 -0600

    Adds resistor_color_duo

---
+++ resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,18 @@
+class ResistorColorDuo 
+  @@table = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }
+  
+  def self.value(arr)
+    return (@@table[arr[0]].to_s + @@table[arr[1]].to_s).to_i 
+  end
+end
