255b966ab5eea4bcc5264f017d8cabb63aa5a18e
normanrs <normanrs@live.com>
2019-04-22 10:36:44 -0600

    Adds prime factors

---
+++ prime-factors/prime_factors.rb
@@ -0,0 +1,13 @@
+require 'prime'
+class PrimeFactors 
+  def self.for(num)
+    result = []
+    if (num.is_a? Integer) && (num >= 2) 
+      Prime.prime_division(num).each do |primes| 
+        primes[1].times { result << primes[0] }
+      end
+    end
+    result
+  end
+
+end
