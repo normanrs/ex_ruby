7e5be5ec453a47276c448bcf51cbe6fa1050956a
Norm Schultz <schultzn@aetna.com>
2020-01-31 16:31:15 -0700

    Armstrong numbers FTW

---
+++ armstrong-numbers/armstrong_numbers.rb
@@ -0,0 +1,26 @@
+=begin
+Write your code for the 'Armstrong Numbers' exercise in this file. Make the tests in
+`armstrong_numbers_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/armstrong-numbers` directory.
+=end
+
+class ArmstrongNumbers 
+
+  def self.include?(num)
+    num == calculate_armstrong(num)
+  end
+
+  def self.calculate_armstrong(num)
+    numbers = num.to_s.chars.map(&:to_i)
+    power   = numbers.count
+    sum     = 0
+    numbers.each do |number| 
+      sum += number**power
+    end
+    sum
+  end
+
+end
+
