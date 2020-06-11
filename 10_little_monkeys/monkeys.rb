41152dab8e984989c6b90d75b77be98e5bbe4dc0
Norm Schultz <schultzn@aetna.com>
2020-06-11 08:31:02 -0600

    Update monkeys.rb

--- 10_little_monkeys/monkeys.rb
+++ 10_little_monkeys/monkeys.rb
@@ -1,3 +1,5 @@
+# 10 Little Monkeys exercise
+
 def number_words 
   { 10 => 'Ten', 9 => 'Nine', 8 => 'Eight', 7 => 'Seven', 6 => 'Six', 5 => 'Five', 4 => 'Four', 3 => 'Three', 2 => 'Two', 1 => 'One' }
 end
