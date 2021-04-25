4144607f3c15d142d598d760bd8a2c5e0dc427ca
Norman Schultz <>
2021-04-25 11:36:56 -0600

    Created shortened urls

---
+++ url_shorten2/shortener.rb
@@ -0,0 +1,40 @@
+require 'csv'
+
+class Shortener
+
+  def urls
+    CSV.read('urls.csv').flatten
+  end
+
+  def alphanums
+    [*'0'..'9', *'a'..'z']
+  end
+
+  def alphanumeric_combinations(num)
+    alphanums.combination(num).map(&:join)
+  end
+
+  def combinations_needed
+    tries = 0
+    count = 0
+    while urls.count > count
+      count = alphanumeric_combinations(tries + 1).count
+      tries += 1
+    end
+    tries
+  end
+
+  def shortened_urls
+    result = {}
+    codes = alphanumeric_combinations(combinations_needed)
+    urls.each do |url|
+      result[url] = "http://www.normed_urls/#{codes.pop}"
+    end
+    result
+  end
+
+end
+
+# all combinations of alphanumeric characters of a certain length
+# determine how many codes you need
+# get array of codes
--- url_shorten2/shortener_test.rb
+++ url_shorten2/shortener_test.rb
@@ -8,4 +8,35 @@ class ThingTest < Minitest::Test
     assert_instance_of Shortener, shortener
   end
 
+  def test_shortener
+    shortener = Shortener.new
+    urls = shortener.urls
+    refute_empty urls
+    assert urls.first.include? 'http://'
+  end
+
+  def test_alphanums
+    shortener = Shortener.new
+    alphanums = shortener.alphanums
+    assert_equal 36, alphanums.count
+  end
+
+  def test_combinations
+    shortener = Shortener.new
+    combinations = shortener.alphanumeric_combinations(2)
+    assert_equal 630, combinations.count
+    assert_equal combinations.count, combinations.uniq.count
+  end
+
+  def test_combinations_needed
+    shortener = Shortener.new
+    needed = shortener.combinations_needed
+  end
+
+  def test_combinations_needed
+    shortener = Shortener.new
+    shortened_urls = shortener.shortened_urls
+    assert_equal shortener.urls.count, shortened_urls.count
+  end
+
 end
