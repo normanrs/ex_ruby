224b94abace44871f5b15012d827d7b52b86a04d
Norman Schultz <>
2021-04-25 10:36:15 -0600

    Create url shortener

---
+++ url_shorten1/shortener.rb
@@ -0,0 +1,36 @@
+require 'csv'
+
+class Shortener
+  # LETTERS = ('a'..'z').to_a
+  # NUMBERS = ('0'..'9').to_a
+  # ALPHANUMERIC = LETTERS + NUMBERS
+  ALPHANUM = [*?a..?z, *?0..?9]
+
+  def urls
+    CSV.read('urls.csv').flatten
+  end
+
+  def write_shortened
+    result = {}
+    codes = permutations(digits_needed)
+    urls.map do |url|
+      result[url] = codes.pop
+    end
+    result
+  end
+
+  def digits_needed
+    attempts = 0
+    count = 0
+    while urls.count > count do
+      count = permutations(attempts).count
+      attempts += 1
+    end
+    attempts - 1
+  end
+
+  def permutations(num)
+    ALPHANUM.repeated_permutation(num + 1).map(&:join)
+  end
+
+end
---
+++ url_shorten1/shortener_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require_relative 'shortener.rb'
+
+class ShortenerTest < Minitest::Test
+
+  def test_item_creation
+    thing = Shortener.new
+    assert_instance_of Shortener, thing
+  end
+
+  def test_reads_urls_from_csv
+    shortener = Shortener.new
+    url_list = shortener.urls
+    refute_empty url_list
+  end
+
+  def test_add_shortened_urls
+    shortener = Shortener.new
+    urls = shortener.urls
+    site_list = shortener.write_shortened
+    refute_empty site_list
+    assert site_list.values.count == site_list.values.uniq.count
+    assert_equal site_list.count, urls.count
+  end
+
+end
